fe8b05e48ca47c958763fec6398263729df1f21e
Kaouthar Kouaz <kkouaz@e1r11p7.1337.ma>
2025-03-02 16:22:48 +0000

    ok

--- hashmap.py
+++ hashmap.py
@@ -2,7 +2,26 @@
 
 my_map = {}
 
-my_map["key"] = "value"
-my_map["key"] = 9
+my_map["keyyy"] = "value"
+my_map["keyy"] = 9
 print(my_map)
 # we cant have duplicates keyy
+my_mao = {"alice" : 90, "bob ": 85}
+print(my_mao)
+
+
+# dict comprehension 
+mapp = { i : i for i in range(5)}
+print(mapp)
+
+
+# loopig through maps
+for key in my_map :
+    print(key, my_map[key])
+
+for val in my_mao.values() :
+    print(val)
+
+for key, val in my_mao.items() :
+    print(key, val)
+
---
+++ tuples.py
@@ -0,0 +1,8 @@
+# tuples are similar than arrays
+tup=(1,2,3)
+print(tup)
+
+# they are immutable, we can index them but we can't modify them
+# can be used as keys for a hashmap
+my_map = {(1, 2) : "Ok"}
+print(my_map)
